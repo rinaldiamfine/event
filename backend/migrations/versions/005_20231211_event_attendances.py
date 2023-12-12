91ee82230d42705fed64a4263de3769dcad1b65d
Rinaldi <rinaldiamfine@gmail.com>
2023-12-12 11:11:00 +0700

    Test

--- backend/migrations/versions/005_20231211_event_attendances.py
+++ backend/migrations/versions/005_20231211_event_attendances.py
@@ -1,6 +1,6 @@
 """empty message
 
-Revision ID: 8ff56df3e95b
+Revision ID: 005
 Revises: 005
 Create Date: 2023-12-11 19:24:48.118928
 
@@ -10,27 +10,29 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '8ff56df3e95b'
+revision = '005'
 down_revision = '004'
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    op.create_table(
-        'event_attendances',
-        sa.Column('created', sa.DateTime(), nullable=True),
-        sa.Column('updated', sa.DateTime(), nullable=True),
-        sa.Column('id', sa.Integer(), nullable=False),
-        sa.Column("event_line_id", sa.Integer, sa.ForeignKey("event_lines.id"), nullable=False),
-        sa.Column("event_registration_id", sa.Integer, sa.ForeignKey("event_registrations.id"), nullable=False),
-        sa.Column('date_claim', sa.DateTime(), nullable=False),
-        sa.Column('created_uid', sa.Integer(), nullable=False),
-        sa.Column('updated_uid', sa.Integer(), nullable=False),
-        sa.Column('is_deleted', sa.Boolean(), nullable=True),
-        sa.Column('deleted', sa.DateTime(), nullable=True),
-        sa.PrimaryKeyConstraint('id')
-    )
+    # op.create_table(
+    #     'event_attendances',
+    #     sa.Column('created', sa.DateTime(), nullable=True),
+    #     sa.Column('updated', sa.DateTime(), nullable=True),
+    #     sa.Column('id', sa.Integer(), nullable=False),
+    #     sa.Column("event_line_id", sa.Integer, sa.ForeignKey("event_lines.id"), nullable=False),
+    #     sa.Column("event_registration_id", sa.Integer, sa.ForeignKey("event_registrations.id"), nullable=False),
+    #     sa.Column('date_claim', sa.DateTime(), nullable=False),
+    #     sa.Column('created_uid', sa.Integer(), nullable=False),
+    #     sa.Column('updated_uid', sa.Integer(), nullable=False),
+    #     sa.Column('is_deleted', sa.Boolean(), nullable=True),
+    #     sa.Column('deleted', sa.DateTime(), nullable=True),
+    #     sa.PrimaryKeyConstraint('id')
+    # )
+    pass
 
 def downgrade():
-    op.drop_table('event_attendances')
+    # op.drop_table('event_attendances')
+    pass
