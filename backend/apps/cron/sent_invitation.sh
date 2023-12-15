0f5fc5bbc11a7446df113309021ba130e29155ed
Rinaldi <rinaldiamfine@gmail.com>
2023-12-15 12:08:41 +0700

    Update the manage.py and adding cron

---
+++ backend/apps/cron/sent_invitation.sh
@@ -0,0 +1,7 @@
+## // For every day at 00:00
+# 0 0 * * * (COMMAND TO RUN ex: python3 manage.py xxxx
+
+## This script below need to paste on bash (dont forget to add script to activate the virtualenv)
+python3 manage.py sent_invitation 1
+
+# Pleasse check the .env path to run the python script
