e70071959a47c2e883c9d154caa773b8df706856
Rinaldi <rinaldiamfine@gmail.com>
2023-12-15 20:58:23 +0700

    Finalize email template v2

---
+++ backend/migrations/versions/006_20231215_authentication.py
@@ -0,0 +1,24 @@
+"""Create user for authentication
+
+Revision ID: 006
+Revises: 005
+Create Date: 2023-12-15 20:02:40.159481
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '006'
+down_revision = '005'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
